413085cc39ead6ce4eacb333ac15df08dabdebb0
EduardF1 <fischer_eduard@yahoo.com>
2021-10-09 15:16:51 +0200

    and internationalization and admin dashboard

---
+++ lesson_14_symfony_project/src/Controller/Admin/DashboardController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use EasyCorp\Bundle\EasyAdminBundle\Router\CrudUrlGenerator;
+
+/**
+ * @property AdminUrlGenerator $adminUrlGenerator
+ */
+class DashboardController extends AbstractDashboardController
+{
+    public function __construct(AdminUrlGenerator $adminUrlGenerator)
+    {
+        $this->adminUrlGenerator = $adminUrlGenerator;
+    }
+
+    /**
+     * @Route("/admin", name="admin")
+     */
+    public function index(): Response
+    {
+        $url = $this->adminUrlGenerator
+            ->setController(BlogPostCrudController::class)
+            ->generateUrl();
+
+        return $this->redirect($url);
+    }
+}
