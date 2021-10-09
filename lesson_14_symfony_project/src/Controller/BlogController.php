8e26cbe6ccdd09d43a72161e7b390fedf6356586
EduardF1 <fischer_eduard@yahoo.com>
2021-10-09 12:29:41 +0200

    blog controller works now...

--- lesson_14_symfony_project/src/Controller/BlogController.php
+++ lesson_14_symfony_project/src/Controller/BlogController.php
@@ -2,12 +2,19 @@
 
 namespace App\Controller;
 
+use App\Entity\BlogPost;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\Serializer;
+
 
 /**
+ * Class BlogController
+ * @package App\Controller
  * @Route("/blog")
  */
 class BlogController extends AbstractController
@@ -31,41 +38,77 @@ class BlogController extends AbstractController
     ];
 
     /**
-     * @Route("/{page}", name="blog_list", defaults={"page":1})
+     * @Route("/{page}", name="blog_list", defaults={"page": 5}, requirements={"page"="\d+"})
      */
     public function list(Request $request, $page = 1): JsonResponse
     {
         $limit = $request->get('limit', 10);
+        $repository = $this->getDoctrine()->getRepository(BlogPost::class);
+        $items = $repository->findAll();
+
         return $this->json(
             [
                 'page' => $page,
                 'limit' => $limit,
-                'data' => array_map(function ($item) {
-                    //   return $this->generateUrl('blog_by_slug', ['slug' => $item['slug']]);
-                    return $this->generateUrl('blog_by_id', ['id' => $item['id']]);
-                }, self::POSTS)
+                'data' => array_map(function (BlogPost $item) {
+                    return $this->generateUrl('blog_by_slug', ['slug' => $item->getSlug()]);
+                }, $items)
             ]
         );
     }
 
     /**
-     * @Route("/post/{id}", name="blog_by_id", requirements={"id"="\d+"})
+     * @Route("/post/{id}", name="blog_by_id", requirements={"id"="\d+"}, methods={"GET"})
+     * @ParamConverter("post", class="App:BlogPost")
      */
-    public function post($id): JsonResponse
+    public function post($post): JsonResponse
     {
-        return $this->json(
-            self::POSTS[array_search($id, array_column(self::POSTS, 'id'))]
-        );
+        // It's the same as doing find($id) on repository
+        return $this->json($post);
     }
 
     /**
-     * @Route("/{slug}", name="blog_by_slug")
+     * @Route("/post/{slug}", name="blog_by_slug", methods={"GET"})
+     * The below annotation is not required when $post is type-hinted with BlogPost
+     * and route parameter name matches any field on the BlogPost entity
+     * @ParamConverter("post", class="App:BlogPost", options={"mapping": {"slug": "slug"}})
      */
-    public function postBySlug($slug): JsonResponse
+    public function postBySlug(BlogPost $post): JsonResponse
     {
-        return new JsonResponse(
-            self::POSTS[array_search($slug, array_column(self::POSTS, 'slug'))]
-        );
+        // Same as doing findOneBy(['slug' => contents of {slug}])
+        return $this->json($post);
     }
 
+    /**
+     * @Route("/add", name="blog_add", methods={"POST"})
+     */
+    public function add(Request $request): JsonResponse
+    {
+        /** @var Serializer $serializer */
+        $serializer = $this->get('serializer');
+
+        $blogPost = $serializer->deserialize(
+            $request->getContent(),
+            BlogPost::class,
+            'json'
+        );
+
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->persist($blogPost);
+        $entityManager->flush();
+
+        return $this->json($blogPost);
+    }
+
+    /**
+     * @Route("/post/{id}", name="blog_delete", methods={"DELETE"})
+     */
+    public function delete(BlogPost $post): JsonResponse
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->remove($post);
+        $entityManager->flush();
+
+        return new JsonResponse(null, Response::HTTP_NO_CONTENT);
+    }
 }
